afc8b50652b6fcb94d9fbf37b7b82b794a283eb2
Camille Coti <coti@thorim.local>
2013-09-18 23:37:10 +0200

    portable shebang

--- classement.py
+++ classement.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2.6
 
 """ Outil de recuperation du classement.
  " (c) Camille Coti, 2013
--- palmares_urllib.py
+++ palmares_urllib.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2.6
 
 
 """ Outil de recuperation du classement.
