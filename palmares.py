30368cd36d18ab17bc3126209437ed5db31df5bf
Camille Coti <coti@lipn.fr>
2014-10-16 23:32:36 +0200

    Passage sur le palma 2015

--- palmares.py
+++ palmares.py
@@ -309,7 +309,7 @@ def nbVictoiresChamp( tab ):
 
 # Calcule le classement d'un joueur
 def classementJoueur( opener, id, nom, classement, sexe, profondeur ):
-    V, D = getPalma( 2014, id, opener )
+    V, D = getPalma( 2015, id, opener )
     myV = []
     myD = []
     palmaV = []
