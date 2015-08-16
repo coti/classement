14f3893d4d17977447ff900bb9bb4e98d850a8b7
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2015-08-16 20:17:01 +0200

    Revert "Utilisation de la lib requests pour les requêtes HTTP."
    This reverts commit c3b50aeabb950e28ee40d6d1f62d5130e9488c34.

---
+++ gaecookie.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+#
+#####################################################################
+#                                                                   #
+#  File: gaecookie.py                                               #
+#                                                                   #
+#  Copyright (C) 2012 Min Xiang <cff29546@gmail.com>                #
+#                                                                   #
+#  Home: https://gae-cookie-processor.googlecode.com/               #
+#                                                                   #
+#  This file is part of GAECookieProcessor project.                 #
+#                                                                   #
+#  This project is released under the MIT License. See COPYING in   #
+#  the project.                                                     #
+#                                                                   #
+#####################################################################
+
+import re,urllib.request,urllib.error,urllib.parse
+
+# This regex is implemented according to RFC2109, RFC2068 and RFC6265
+NS_COOKIE=re.compile(r'''
+(?P<name>[!#-'*+.0-9a-z^_`|~-]+)             # cookie-name = token (defined in RFC2068)
+= (?P<value>[^\x00-\x20\x7f-\xff",;\\]+ |    # cookie-octet (defined in RFC6265)
+            "(?:[^"\\]|\\.)*"   )            # cookie-value = *cookie-octet | quoted-string
+\s*
+(?P<cookieavs>
+ (;\s*                                       # begin of a cookie-av
+  (?:comment=(?P<comment>                    # comment = token | quoted-string
+      [!#-'*+.0-9a-z^_`|~-]+ | 
+      "(?:[^"\\]|\\.)*")
+    | max-age=(?P<maxage>[0-9]+)             # max-age
+    | domain=(?P<domain>[0-9a-z.-]+)         # domain
+    | path=(?P<path>                         # path
+       [^\x00-\x1f\x7f-\xff;,]+              # path-value in RFC6265, but with assumption
+      )                                      #   that commas do not exist.
+                                             #   As GAE use ',' as header line separator,
+                                             #   including comma will ruin the path-value
+                                             #   as well as next cookie when path-av is
+                                             #   the last cookie-av. And in common cases
+                                             #   path won't contain any comma
+    | (?P<secure>secure)                     # secure
+    | (?P<httponly>httponly)                 # httponly
+    | version=(?P<version>\d+)               # version
+    | expires=(?P<expires>                   # Expires and rfc1123-date
+       (?:Mon|Tue|Wed|Thu|Fri|Sat|Sun),\s+   # wkday
+       (?P<day>\d+)(?:\s+|[-\/])             # day
+       (?P<month>Jan|Feb|Mar|Apr|May|Jun|Jul # month
+        |Aug|Sep|Oct|Nov|Dec)(?:\s+|[-\/]) 
+       (?P<year>\d+)\s*                      # year
+       (?P<hour>\d\d?):(?P<minute>\d\d):     # time
+       (?P<second>\d\d)?\s* (?:GMT|UTC)      # time zone
+      )
+    | (?P<extensionav>                       # extension-av in RFC6265, but with assumption
+       [^\x00-\x1f\x7f-\xff;,]+              #   that commas do not exist.
+      )                                      #   Same reason as path-value
+  )
+ )*                                          # end of a cookie-av
+)\s*''',re.X | re.I)
+
+# Obsoleted "set-cookie2" headers defined by RFC2965
+# support comma separated cookies by definition
+# so there is nothing to fix with "set-cookie2" cookies
+
+class GAECookieProcessor(urllib.request.HTTPCookieProcessor):
+  def __init__(self, cookiejar=None):
+    import http.cookiejar
+    if cookiejar is None:
+      cookiejar = http.cookiejar.CookieJar()
+    self.cookiejar = cookiejar
+
+  def http_request(self, request):
+    self.cookiejar.add_cookie_header(request)
+    return request
+
+  def http_response(self, request, response):
+    
+    #recover setcookie before extract cookies
+    recovered_hdrs=[]
+    for h in response.info().headers:
+      if h[0:10].lower().startswith('set-cookie'):
+        for c in NS_COOKIE.finditer(h):
+          recovered_hdrs.append('Set-Cookie: %s' % c.group(0))
+      else:
+        recovered_hdrs.append(h)
+    response.info().headers=recovered_hdrs
+
+    self.cookiejar.extract_cookies(response, request)
+    return response
+
+  https_request = http_request
+  https_response = http_response
+
+def test():
+  i=input()
+  while i !='':
+    print(i)
+    for s in NS_COOKIE.finditer(i):
+      print('Set-Cookie: %s' % s.group(0))
+      print(s.group('name'),'=',s.group('value'))
+      print('  path   =',s.group('path'))
+      print('  domain =',s.group('domain'))
+      print('  expires=',s.group('expires'))
+      print('  max-age=',s.group('maxage'))
+    try:
+      i=input()
+    except:
+      i=''
+
+if __name__ == '__main__':
+  test()
--- palmares.py
+++ palmares.py
@@ -15,31 +15,95 @@
 """
 
 
+import urllib.request, urllib.parse, urllib.error
+import urllib.request, urllib.error, urllib.parse
+import http.client
+import http.cookiejar
 import re
-import requests
+import socket
+
+from gaecookie import GAECookieProcessor
+from keepalive import HTTPHandler
 
 from classement import calculClassement, penaliteWO, nbWO
 
 server    = "https://edl.app.fft.fr"
 
-# Effectue une requête avec vérification des erreurs
-def requete( session, url, data=None, timeout=None ):
+# Construit l'opener, l'objet urllib2 qui gere les comm http, et le cookiejar
+def buildOpener():
+    global server
+    headers = {  "Connection" : "Keep-alive",
+                 #'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.65 Safari/537.36',
+                 'User-Agent' : 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/4.0.3 Safari/531.9',
+                'Cache-Control' : 'max-age=0',
+                'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Origin' : server,
+                'Content-Type' : 'application/x-www-form-urlencoded',
+                'Referer' : server + 'espaceclic/connection.do',
+                'Accept-Encoding' : 'gzip,deflate,sdch',
+                'Accept-Language' : 'fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4'
+                }
+
+    policy = http.cookiejar.DefaultCookiePolicy( rfc2965=True )
+    cj = http.cookiejar.CookieJar( policy )
+    keepalive_handler = HTTPHandler()
     try:
-        rep = session.post( url, data=data, timeout=timeout )
-        if rep.status_code != 200:
+        opener = urllib.request.build_opener( keepalive_handler, GAECookieProcessor( cj )  )
+    except urllib.error.URLError as e:
+        print("URL error")
+        if hasattr(e, 'reason'):
+            print('Serveur inaccessible.')
+            print('Raison : ', e.reason)
+        if hasattr(e, 'code'):
             print('Le serveur n\'a pas pu répondre à la requete.')
-            print('Code d\'erreur : ', rep.status_code)
-            if rep.status_code == 403:
+            print('Code d\'erreur : ', e.code)
+            if e.code == 403:
                 print('Le serveur vous a refusé l\'accès')
-            if rep.status_code == 404:
+            if e.code == 404:
                 print('La page demandée n\'existe pas. Peut-être la FFT a-t-elle changé ses adresses ?')
-            exit( -1 )
-        return rep
-    except requests.exceptions.ConnectionError:
-        print('Serveur inaccessible.')
-        print("Verifiez votre connexion, ou l\'état du serveur de la FFT")
         exit( -1 )
-    except requests.exceptions.Timeout:
+    except urllib.error.HTTPError as e:
+        print("HTTP error code ", e.code, " : ", e.reason)
+        print("Vérifiez votre connexion, ou l\'état du serveur de la FFT")
+        exit( -1 )
+    except:
+        import sys
+        print("Autre exception : ", sys.exc_info()[0], sys.exc_info()[1])
+        exit( -1 )
+
+    t_headers = []
+    for k, v in list(headers.items()):
+        t_headers.append( ( k, v ) )
+    opener.addheaders = t_headers 
+
+    return cj, opener
+
+# S'authentifie aupres du serveur
+def authentification( login, password, opener, cj ):
+    global server
+    page      = "/espacelic/connexion.do"
+    payload   = { 'dispatch' : 'identifier', 'login' : login, 'motDePasse' : password }
+    data      = urllib.parse.urlencode( payload ).encode('utf-8')
+    timeout   = 60
+
+    # On ouvre la page d'authentification
+    try:
+        rep = opener.open( server+page, data, timeout )
+    except urllib.error.URLError as e:
+        print("URL error:", e.reason)
+        print("Verifiez votre connexion, ou l\'état du serveur de la FFT")
+        if hasattr(e, 'reason'):
+            print('Serveur inaccessible.')
+            print('Raison : ', e.reason)
+        if hasattr(e, 'code'):
+            print('Le serveur n\'a pas pu répondre à la requete.')
+            print('Code d\'erreur : ', e.code)
+            if e.code == 403:
+                print('Le serveur vous a refusé l\'accès')
+            if e.code == 404:
+                print('La page demandée n\'existe pas. Peut-être la FFT a-t-elle changé ses adresses ?')
+        exit( -1 )
+    except socket.timeout as e:
         print("Timeout -- connexion impossible au serveur de la FFT")
         print("Verifiez votre connexion, ou l\'etat du serveur de la FFT")
         exit( -1 )
@@ -48,28 +112,54 @@ def requete( session, url, data=None, timeout=None ):
         print("Autre exception : ", sys.exc_info()[0], sys.exc_info()[1])
         exit( -1 )
 
-# S'authentifie aupres du serveur
-def authentification( login, password, session ):
-    global server
-    page      = "/espacelic/connexion.do"
-    payload   = { 'dispatch' : 'identifier', 'login' : login, 'motDePasse' : password }
-    timeout   = 60
+    # On recupere alors les cookies, donc on les insere dans l'en-tete http
+    cookietab = []
+    for c in cj:
+        cookietab.append( c.name + '=' + c.value )
+    myCookie = '; '.join( cookietab )
 
-    # On ouvre la page d'authentification
-    requete( session, server+page, payload, timeout )
-
+    opener.addheaders.append( ( "Cache-Control", "no-cache=\"set-cookie, set-cookie2\"" ) )
+    opener.addheaders.append( ( "Cookie", myCookie ) )
+    
     return
 
 # Retourne l'identifiant interne d'un licencie
-def getIdentifiant( session, numLicence ):
+def getIdentifiant( opener, numLicence ):
 
     global server
     page      = "/espacelic/private/recherchelic.do"
     payload   = { 'dispatch' : 'rechercher', 'numeroLicence' : numLicence }
+    data      = urllib.parse.urlencode( payload )
     timeout   = 60
 
-    rep = requete( session, server+page, payload, timeout )
-    line = rep.text
+    try:
+        rep = opener.open( server+page, data, timeout ) 
+        line = rep.read()
+    except urllib.error.URLError as e:
+        print("URL error")
+        if hasattr(e, 'reason'):
+            print('Serveur inaccessible.')
+            print('Raison : ', e.reason)
+        if hasattr(e, 'code'):
+            print('Le serveur n\'a pas pu repondre a la requete.')
+            print('Code d\'erreur : ', e.code)
+            if e.code == 403:
+                print('Le serveur vous a refusé l\'accès')
+            if e.code == 404:
+                print('La page demandée n\'existe pas. Peut-être la FFT a-t-elle changé ses adresses ?')
+        exit( -1 )
+    except urllib.error.HTTPError as e:
+        print("HTTP error code ", e.code, " : ", e.reason)
+        print("Verifiez votre connexion, ou l\'etat du serveur de la FFT")
+        exit( -1 )
+    except socket.timeout as e:
+        print("Timeout -- connexion impossible au serveur de la FFT")
+        print("Verifiez votre connexion, ou l\'etat du serveur de la FFT")
+        exit( -1 )
+    except:
+        import sys
+        print("Autre exception : ", sys.exc_info()[0], sys.exc_info()[1])
+        exit( -1 )
 
     # on parse et on recupere le nom
     r_nom = r'<td class="r_nom">\s*(.*?)\s*</td>'
@@ -107,13 +197,31 @@ def getIdentifiant( session, numLicence ):
     return nom, idu, cl, sexe
 
 # Obtenir le palma d'un joueur d'identifiant donne
-def getPalma( annee, id, session ):
+def getPalma( annee, id, opener ):
     global server
     page      = "/espacelic/private/palmares.do"
     payload = { 'identifiant' : id, 'millesime' : annee }
+    data = urllib.parse.urlencode( payload )
 
-    rep = requete( session, server+page, payload )
-    line = rep.text
+    try:
+        rep = opener.open( server+page, data ) 
+        line = rep.read()
+    except urllib.error.URLError as e:
+        print("URL error:", e.reason)
+        print("Vérifiez votre connexion, ou l\'état du serveur de la FFT")
+        exit( -1 )
+    except urllib.error.HTTPError as e:
+        print("HTTP error code ", e.code, " : ", e.reason)
+        print("Vérifiez votre connexion, ou l\'état du serveur de la FFT")
+        exit( -1 )
+    except socket.timeout as e:
+        print("Timeout -- connexion impossible au serveur de la FFT")
+        print("Vérifiez votre connexion, ou l\'état du serveur de la FFT")
+        exit( -1 )
+    except:
+        import sys
+        print("Autre exception : ", sys.exc_info()[0], sys.exc_info()[1])
+        exit( -1 )
 
     # Separation victoires/defaites
 
@@ -200,8 +308,8 @@ def nbVictoiresChamp( tab ):
     return nb
 
 # Calcule le classement d'un joueur
-def classementJoueur( session, id, nom, classement, sexe, profondeur ):
-    V, D = getPalma( 2015, id, session )
+def classementJoueur( opener, id, nom, classement, sexe, profondeur ):
+    V, D = getPalma( 2015, id, opener )
     myV = []
     myD = []
     palmaV = []
@@ -231,13 +339,13 @@ def classementJoueur( session, id, nom, classement, sexe, profondeur ):
 
         # calcul du futur classement de mes victoires
         for _v in V:
-            nc,harm = classementJoueur( session, _v[1], _v[0], _v[2], sexe, profondeur )
+            nc,harm = classementJoueur( opener, _v[1], _v[0], _v[2], sexe, profondeur )
             myV.append( ( nc, _v[3] ) )
             palmaV.append( ( _v[0], _v[2], nc, _v[3] ) )
 
         # calcul du futur classement de mes defaites
         for _d in D:
-            nc,harm = classementJoueur( session, _d[1], _d[0], _d[2], sexe, profondeur )
+            nc,harm = classementJoueur( opener, _d[1], _d[0], _d[2], sexe, profondeur )
             myD.append( ( nc, _d[3] ) )
             palmaD.append( ( _d[0], _d[2], nc, _d[3] ) )
 
@@ -273,24 +381,11 @@ def classementJoueur( session, id, nom, classement, sexe, profondeur ):
 
 def recupClassement( login, password, LICENCE, profondeur ):
     
-    session = requests.session()
-    session.headers = {
-        "Connection" : "Keep-alive",
-        #'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.65 Safari/537.36',
-        'User-Agent' : 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/4.0.3 Safari/531.9',
-        'Cache-Control' : 'max-age=0',
-        'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Origin' : server,
-        'Content-Type' : 'application/x-www-form-urlencoded',
-        'Referer' : server + 'espaceclic/connection.do',
-        'Accept-Encoding' : 'gzip,deflate,sdch',
-        'Accept-Language' : 'fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4'
-    }
-
     # On s'identifie et on obtient ses prores infos
-    authentification( login, password, session )
+    cj, op = buildOpener()
+    authentification( login, password, op, cj )
 
-    nom, id, cl, sexe = getIdentifiant( session, LICENCE )
+    nom, id, cl, sexe = getIdentifiant( op, LICENCE )
     print(nom)
     print(id)
     print(cl)
@@ -298,7 +393,7 @@ def recupClassement( login, password, LICENCE, profondeur ):
 
     # recuperation de son propre palma, et recursivement de celui des autres
 
-    new_cl, harm = classementJoueur( session, id, nom, cl, sexe, profondeur )
+    new_cl, harm = classementJoueur( op, id, nom, cl, sexe, profondeur )
 
     print("nouveau classement: ", harm, " (après harmonisation) - ", new_cl, " (calculé)")
     return
